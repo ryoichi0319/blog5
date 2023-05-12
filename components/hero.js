b90d20895cd71c09ff5499d56e6fd04ea0dde2c7
losegroove <losegroove@icloud.com>
2023-05-12 21:51:47 +0900

    0512

--- components/hero.js
+++ components/hero.js
@@ -1,13 +1,25 @@
 import styles from "styles/hero.module.css"
-
-export default function Hero({title, subtitle, imageOn=false}){
+import Image from "next/legacy/image"
+import cube from "images/cube.jpg"
+export default function Hero({title, subtitle, imageOn = false}){
     return(
         <div className={styles.flexContainer}>
           <div className={styles.text}>
             <h1 className={styles.title}>{title}</h1>
             <p className={styles.subtitle}>{subtitle}</p>
-            {imageOn && <figure>[画像]</figure>}
+            168 124 171
           </div> 
+          {imageOn && (
+            <figure className={styles.image}>
+              <Image src={cube}
+               alt=""
+               layout="responsive"
+               sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
+               priority
+               placeholder="blur" 
+               style={{transition:"0.2s"}}/>
+            </figure>
+          )}
         </div>
     )
 }
